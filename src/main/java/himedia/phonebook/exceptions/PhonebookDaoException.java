97c49e3e040e0c339ecd00751beed481693f0302
jensen-kor <lukawa1@naver.com>
2025-02-13 11:20:12 +0900

    111

---
+++ src/main/java/himedia/phonebook/exceptions/PhonebookDaoException.java
@@ -0,0 +1,24 @@
+package himedia.phonebook.exceptions;
+
+import himedia.phonebook.repository.vo.PhonebookVo;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhonebookDaoException extends RuntimeException {
+	private PhonebookVo phonebookVo = null;
+
+	public PhonebookDaoException(String message, 
+								Throwable cause) {
+		super(message, cause);
+	}
+	public PhonebookDaoException(String message,
+								Throwable cause,
+								PhonebookVo vo) {
+		super(message, cause);
+		this.phonebookVo = vo;
+	}
+	
+	
+}
